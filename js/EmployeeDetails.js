dff5a62df2880fe124f02e65c306522c75663c4b
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-12-21 12:26:36 +0530

    ADD:Employee Payroll form

---
+++ js/EmployeeDetails.js
@@ -0,0 +1,52 @@
+class EmployeeDetails{
+    get name(){
+        return this._name;
+    }
+    set name(name){
+        this._name = name;
+    }
+    // get profileImage(){
+    //     return 
+    // }
+    
+    // set profile(image){
+        
+    // }
+
+    // get gender(){
+
+    // }
+    // set genderDetails(gender){
+
+    // }
+    // get dept(){
+
+    // }
+    // set department(department){
+
+    // }
+    // get sal(){
+
+    // }
+    // set salary(salary){
+
+    // }
+    
+    // get strtDate(){
+
+    // }
+    // set startDate(startDate){
+
+    // }
+
+    // get note(){
+
+    // }
+
+    // set notes(notes){
+
+    // }
+
+}
+
+ export default EmployeeDetails;
---
+++ js/emp.js
@@ -0,0 +1,6 @@
+import EmployeeDetails from "./EmployeeDetails.js";
+
+let empDetails = new EmployeeDetails();
+
+empDetails.name = 'Madhu';
+console.log(empDetails.name());
